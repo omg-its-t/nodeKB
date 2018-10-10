a82ca22266d40d9efa6ca96169a8fd38b267f11f
Theo Mellske <blackvq35@gmail.com>
2018-10-09 23:05:43 -0500

    added user registration process

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 
 //bring in User model
 let User = require('../models/users');
@@ -12,5 +13,62 @@ router.get('/register', function(req, res){
   });
 });
 
+//register process (submitting a post to this route)
+router.post('/register', function(req,res){
+  const name = req.body.name;
+  const email = req.body.email;
+  const username = req.body.username;
+  const password = req.body.password;
+  const password2 = req.body.password2;
+
+  req.checkBody('name', 'Your name is required.').notEmpty();
+  req.checkBody('email', 'Your email is required.').notEmpty();
+  req.checkBody('email', 'Email must be a valid address.').isEmail();
+  req.checkBody('username', 'A username is required.').notEmpty();
+  req.checkBody('password', 'A password is required.').notEmpty();
+  req.checkBody('password2', 'Passwords do not match!').equals(req.body.password);
+
+  let errors = req.validationErrors();
+
+  //if errors re-render template and pass along erros
+  if (errors){
+    res.render('register',{
+      errors:errors
+    });
+  }
+  //if no errors create user object, hash password and save to DB
+  else{
+    let newUser = new User({
+      name:name,
+      email:email,
+      username:username,
+      password:password,
+    });
+
+    bcrypt.genSalt(10, function(err, salt){
+      bcrypt.hash(newUser.password, salt, function(err, hash){
+        if (err){
+          colsole.log(err);
+        }
+        newUser.password = hash;
+        newUser.save(function(err){
+          if(err){
+            console.log(err);
+            return;
+          }
+          else{
+            req.flash('Success', 'You are now registered');
+            res.redirect('/users/login');
+          }
+        });
+      });
+    });
+  }
+});
+
+//login route
+router.get('/login', function(req, res){
+  res.render('login');
+})
 // so we can access the route from outside
 module.exports = router;
